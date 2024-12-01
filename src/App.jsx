6e80277027ee6feec32f195fc54eb178468a53d8
SawantAchal <achals1718@gmail.com>
2024-12-02 01:47:09 +0530

    push package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
+import Homepage from './pages/Homepage'
+
 
 function App() {
 
   return (
     <>
-
+      <Homepage/>
+      <Routes>
+      
+      </Routes>
     </>
   )
 }
