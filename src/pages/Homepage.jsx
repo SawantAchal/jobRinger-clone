8bd4f5fee04edefefc2ecbaf3bbbe05b1c96e690
SawantAchal <achals1718@gmail.com>
2024-12-02 00:47:21 +0530

    mount browseJob component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import BrowseJob from '../components/BrowseJob'
 
 const Homepage = () => {
   return (
@@ -10,6 +11,7 @@ const Homepage = () => {
         <Navbar/>
         <Header/>
       </div>
+      <BrowseJob/>
       <Footer/>
     </>
   )
