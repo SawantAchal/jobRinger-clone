b562a42ed72d7cfbb98c1aa0aa5f861ebe52a29e
SawantAchal <achals1718@gmail.com>
2024-12-01 23:22:10 +0530

    mounted header and navbar component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+
+const Homepage = () => {
+  return (
+    <>
+      <div className='bg-[#CEFFE8] h-screen'>
+        <Navbar/>
+        <Header/>
+      </div>
+    </>
+  )
+}
+
+export default Homepage
