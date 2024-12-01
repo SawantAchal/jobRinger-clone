aae5c470eb6471e0130ccbe5e470d6b774cca5ac
SawantAchal <achals1718@gmail.com>
2024-12-02 01:56:29 +0530

    push package

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the build files are placed in the 'dist' folder
+    rollupOptions: {
+      input: 'index.html', // Ensure Vite knows where the entry point is
+    }
+  },
 })
